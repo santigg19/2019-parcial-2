243ec1038c7f7340124b3d647847f58e6bdbb57d
santigg19 <48955127+santigg19@users.noreply.github.com>
2019-06-04 11:15:57 -0300

    Add files via upload
    Parcial 2

---
+++ 58157-SantiagoGraffigna/Parcial2/2019-parcial-2/telefono.py
@@ -0,0 +1,49 @@
+class Telefono(object):
+
+    
+    def __init__(self):
+        self.credito = 0
+        self.llamada_ok = False
+        
+    
+    def dial(self, numero):
+       
+        if len(numero) == 3:
+
+            if (numero[0] == '9' or numero[0] == '8'):
+
+                
+                return True
+
+            else:
+
+                return False
+        
+        if len(numero) == 7:
+           
+            if self.credito < 10:
+
+                return False
+
+            else:
+
+                self.credito -= 10
+                
+                return True
+        
+        
+        if numero[0:2] == '00':
+
+            if self.credito < 100:
+
+                return False
+            
+            else:
+            
+                self.credito -= 100
+            
+            return True
+
+    def agregar_credito(self,credito):
+       
+        self.credito += credito
