4d205e0f59d6488393137d682c4efbb16cbe2c53
lucasfozzatti <fozzatti98@gmail.com>
2019-06-04 11:00:52 -0300

    entrega 2do parcial

---
+++ 58188 - Lucas Fozzatti/test_printer.py
@@ -0,0 +1,13 @@
+import unittest
+from printer import printer
+
+
+class TestPrinter(unittest.TestCase):
+def test.printer(self):
+   pass
+   def test_printer_ok(self)
+   printer = printer[]
+   self.asserTrue(print.)
+
+
+
